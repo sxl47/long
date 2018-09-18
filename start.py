7b39d36ec087e3c0b0dc019390fd984f9a5b4ec5
sxl <sxl47@126.com>
2018-09-18 21:16:52 +0800

    增加启动脚本

---
+++ start.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: 1.0
+@author: sxl
+@file: start.py
+@time: 2018/9/18 21:16:03
+@desc:
+
+"""
+
+
+def start():
+    pass
+
+
+if __name__ == '__main__':
+    start()
