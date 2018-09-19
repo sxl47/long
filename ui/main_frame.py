8281c19b397cfb3553c81f36dcf70355ac003bb5
sxl <sxl47@126.com>
2018-09-19 21:22:11 +0800

    增加样式

--- ui/main_frame.py
+++ ui/main_frame.py
@@ -19,6 +19,36 @@ from ui.main_frame_ui import Ui_main_frame
 class MainFrame(QWidget):
     signal_append_log = pyqtSignal(str)
 
+    qss_style = """
+#main_frame {
+	background-color: rgb(255, 255, 255);
+}
+
+#scrollArea {
+    border-style: solid;
+    border-width: 1px;
+	border-color: rgb(168, 168, 168);
+}
+
+#scrollAreaWidgetContents {
+	background-color: rgb(244, 244, 244);
+}
+
+QPushButton {
+	margin:0px 1px 1px 0px;
+    border-style: solid;
+    border-color: rgb(0,0,0);
+    border-width: 1px;
+    border-radius: 5px;
+	background-color: rgb(90, 154, 250);
+	color: rgb(255, 255, 255);
+}
+QPushButton:pressed {
+	margin:1px 0px 0px 1px;
+	background-color: rgb(110, 174, 255);
+}
+"""
+
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         self.ui = None
@@ -29,7 +59,9 @@ class MainFrame(QWidget):
     def init(self):
         self.ui = Ui_main_frame()
         self.ui.setupUi(self)
-        self.resize(1024, 750)
+        self.resize(400, 300)
+
+        self.setStyleSheet(self.qss_style)
 
         # 日志
         # self.signal_append_log.connect(self.ui.textEdit_log.append)
